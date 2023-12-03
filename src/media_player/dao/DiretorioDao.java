28cc854416eccf09aaa6ec0b46238558daf564ab
Your Name <you@example.com>
2023-12-02 21:26:56 -0300

    added methods

--- src/media_player/dao/DiretorioDao.java
+++ src/media_player/dao/DiretorioDao.java
@@ -1,18 +1,71 @@
 package media_player.dao;
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class DiretorioDao {
+    private ArrayList<String> diretorios;
+
     public void cadastrarDiretorio(String caminho) throws IOException {
         String caminhoArquivo = "arquivos/diretorios.txt";
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(caminhoArquivo))) {
             writer.write(caminho);
             writer.newLine();
+            diretorios.add(caminho);
         } 
         catch (IOException e) {
             throw e;
         }
+    }
+
+    public void carregarDiretorios() throws IOException {
+        String caminhoArquivo = "arquivos/diretorios.txt";
+        File arquivo = new File(caminhoArquivo);
+
+        if (!arquivo.exists()){
+            arquivo.getParentFile().mkdirs();
+        }
+
+        try(BufferedReader br = new BufferedReader(new FileReader(caminhoArquivo))) {
+            String linha;
+            while((linha = br.readLine()) != null) {
+                diretorios.add(linha);
+            }
+        }
+        catch(IOException e) {
+            throw e;
+        }
+    }
+
+    public void escreverMusicas() throws IOException {
+        String caminhoMusicas = "arquivos/musicas.txt";
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(caminhoMusicas))) {
+            for(String caminho : diretorios) {
+                File f = new File(caminho);
+                File[] arquivos = f.listFiles();
+
+                for(File arquivo : arquivos) {
+                    String caminhoArquivo = arquivo.getAbsolutePath();
+                    int indicePonto = caminhoArquivo.lastIndexOf(".");
+
+                    if(indicePonto >= 0 && indicePonto < caminhoArquivo.length()) {
+                        if (caminhoArquivo.substring(indicePonto).equals("mp3")) {
+                            writer.write(caminhoArquivo);
+                            writer.newLine();
+                        }
+                    }
+                }
+            }
+        } 
+        catch (IOException e) {
+            throw e;
+        }
+        
+
     }
 
 }
--- src/media_player/dao/MusicaDao.java
+++ src/media_player/dao/MusicaDao.java
@@ -1,5 +1,36 @@
 package media_player.dao;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import media_player.modelo.Musica;
+
 public class MusicaDao {
-    
+    ArrayList<Musica> musicas;
+
+    public void carregarMusicas() throws IOException {
+        String caminhoArquivo = "arquivos/musicas.txt";
+        File arquivo = new File(caminhoArquivo);
+
+        if (!arquivo.exists()){
+            arquivo.getParentFile().mkdirs();
+        }
+
+        try(BufferedReader br = new BufferedReader(new FileReader(caminhoArquivo))) {
+            String linha;
+            while((linha = br.readLine()) != null) {
+                int indicePonto = linha.lastIndexOf(".");
+                String nomeMusica = linha.substring(0, indicePonto);
+                
+                Musica m = new Musica(nomeMusica, linha);
+                musicas.add(m);
+            }
+        }
+        catch(IOException e) {
+            throw e;
+        }
+    }
 }
